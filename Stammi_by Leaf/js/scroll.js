33f953856f128ec40e3f8f8690dc415c8617478a
Arnav Singh <59086777+ArnavS59@users.noreply.github.com>
2020-03-27 23:44:26 +0100

    Add files via upload
    - Designed and coded a home-page template for Stammi_by leaf , a startup

---
+++ Stammi_by Leaf/js/scroll.js
@@ -0,0 +1,8 @@
+//Jquery and javascript
+//required for the scroll to work
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
